6b7a9fe886518ed443ae3017d751b5ce7dc126a3
zephyrLucas <zephyr@lucas.net>
2013-12-10 09:37:53 -0500

    BasicMovement
    Movement started by reference to aliens at bottom.  Platform Generator
    created although is not functional.  Camra parented to aliens.

---
+++ SuperKick/Assets/AlienScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienScript : MonoBehaviour {
+
+	public static float currentHeight;
+
+	private Vector2 newPos;
+
+	// Use this for initialization
+	void Start () {
+		currentHeight = 0f;
+		newPos = new Vector2(0f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float increase = Time.deltaTime * 2f;
+		currentHeight += increase;
+		newPos.y = currentHeight;
+		transform.localPosition = newPos;
+	}
+}
---
+++ SuperKick/Assets/PlatformGenorator/PlatfromGenorationScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatfromGenorationScript : MonoBehaviour {
+
+	public Transform platformPrefab;
+
+	private ArrayList platforms = new ArrayList();
+
+	public Vector2 currentCamPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void creatTheGround() {
+		if(platforms.Count < 14) {
+			Transform temp = (Transform) Instantiate(platformPrefab);
+			platforms.Add(temp);
+
+		}
+	}
+
+}
