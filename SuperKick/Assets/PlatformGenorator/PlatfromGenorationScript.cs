02445bac6801fcf7e61c17366b85e0371f388f10
zephyrLucas <zephyr@lucas.net>
2013-12-11 08:49:40 -0500

    Code_For_Platforms
    Platforms are now randomly Generator.

--- SuperKick/Assets/PlatformGenorator/PlatfromGenorationScript.cs
+++ SuperKick/Assets/PlatformGenorator/PlatfromGenorationScript.cs
@@ -7,23 +7,44 @@ public class PlatfromGenorationScript : MonoBehaviour {
 
 	private ArrayList platforms = new ArrayList();
 
-	public Vector2 currentCamPos;
+	public Vector2 nextPos;
 
 	// Use this for initialization
 	void Start () {
-	
+		for(int i = 0; i < 14; i++) {
+			innitialcreate();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		createTheGround();
+		destroyTheGround();
 	}
 
-	private void creatTheGround() {
+	private void innitialcreate() {
+		Transform temp = (Transform) Instantiate(platformPrefab);
+		platforms.Add(temp);
+		nextPos.x = Random.Range(-10, 10);
+		nextPos.y = (float) (Random.Range(0, 10) + ((int) AlienScript.currentHeight));
+		temp.localPosition = nextPos;
+	}
+
+	private void createTheGround() {
 		if(platforms.Count < 14) {
 			Transform temp = (Transform) Instantiate(platformPrefab);
 			platforms.Add(temp);
+			nextPos.x = Random.Range(-10, 10);
+			nextPos.y = (float) (Random.Range(10, 20) + ((int) AlienScript.currentHeight));
+			temp.localPosition = nextPos;
 
+		}
+	}
+
+	private void destroyTheGround() {
+		if(((Transform) platforms[0]).localPosition.y < AlienScript.currentHeight) {
+			GameObject.Destroy( ((Transform) platforms[0]).gameObject);
+			platforms.RemoveAt(0);
 		}
 	}
 
