1bb4b7d6a4d7ccae4aa5b59899b0bcea6d52c59a
ChrisHorrigan <bor3d.mad@gmail.com>
2013-12-12 14:35:21 -0500

    PlayerCode
    Super class and a sub class

---
+++ SuperKick/Assets/Players/Player1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : Player {
+	
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		startPos.x=-5;
+		startPos.y=6;
+		transform.localPosition = startPos;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
