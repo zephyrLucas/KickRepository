f82e8d3b15d6adafc492f752278ba857015313c0
zephyrLucas <zephyr@lucas.net>
2013-12-13 14:05:48 -0500

    Rocks_Move
    Rocks are now put into the scene and can move around.

--- SuperKick/Assets/Rocks/RockCode.cs
+++ SuperKick/Assets/Rocks/RockCode.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class RockCode : MonoBehaviour {
 
+
+	Vector2 currentVelocity;
+	Vector2 nextPos;
 	// Use this for initialization
 	void Start () {
-	
+		nextPos = this.transform.localPosition;
+		currentVelocity.x = Random.Range(-3, 3);
+		currentVelocity.y = Random.Range(-3, 3);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		nextPos = nextPos + (Time.deltaTime * currentVelocity);
+		this.transform.localPosition = nextPos;
 	}
 }
--- SuperKick/Assets/Rocks/RockThrower.cs
+++ SuperKick/Assets/Rocks/RockThrower.cs
@@ -5,6 +5,10 @@ public class RockThrower : MonoBehaviour {
 
 	public Transform rockPrefab;
 	private float time;
+
+	private Vector2 nextPos;
+
+	private ArrayList rocks = new ArrayList();
 	// Use this for initialization
 	void Start () {
 	
@@ -12,6 +16,26 @@ public class RockThrower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		meteorThrower();
+		time += Time.deltaTime;
+		if (time > 10f) {
+			GameObject.Destroy( ((Transform) rocks[0]).gameObject);
+			rocks.RemoveAt(0);
+			time = 0f;
+		}
 
 	}
+
+	private void meteorThrower() {
+		if(rocks.Count < 3) {
+			Transform temp = (Transform) Instantiate(rockPrefab);
+			rocks.Add(temp);
+			nextPos.x = Random.Range(-10, 10);
+			nextPos.y = (float) (Random.Range(10, 20) + ((int) AlienScript.currentHeight));
+			temp.localPosition = nextPos;
+		}
+	}
+	
+
+
 }
