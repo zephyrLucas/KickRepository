d3dd5fde15ff7c9a4fcb327c76218a0ebe92d066
ChrisHorrigan <bor3d.mad@gmail.com>
2014-01-13 10:46:45 -0500

    Follower code
    not actually working but here it is

---
+++ SuperKick/Assets/Players/Follower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+//not working!!!
+public class Follower : MonoBehaviour {
+	public Transform target;
+	public Vector2 positionyo;
+	// Use this for initialization
+	void Start () {
+		positionyo.y = 5;
+		positionyo.x = target.localPosition.x; 
+		renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target.localPosition.y > 5) {
+				renderer.enabled = true;
+			} 
+		else
+			renderer.enabled = false;
+		transform.localPosition = positionyo;
+	}
+}
