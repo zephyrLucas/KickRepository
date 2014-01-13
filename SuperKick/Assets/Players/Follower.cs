226ce15126c2837695f413613ba3058c2e9f125f
ChrisHorrigan <bor3d.mad@gmail.com>
2014-01-13 11:05:36 -0500

    Follower Works
    When you get off screen it follows your guy so you know where you will
    land

--- SuperKick/Assets/Players/Follower.cs
+++ SuperKick/Assets/Players/Follower.cs
@@ -6,14 +6,17 @@ public class Follower : MonoBehaviour {
 	public Vector2 positionyo;
 	// Use this for initialization
 	void Start () {
-		positionyo.y = 5;
-		positionyo.x = target.localPosition.x; 
+		positionyo.x = target.localPosition.x;
+		positionyo.y = AlienScript.currentHeight+7;
+
 		renderer.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (target.localPosition.y > 5) {
+		positionyo.y = AlienScript.currentHeight+10;
+		positionyo.x = target.localPosition.x; 
+		if (target.localPosition.y > AlienScript.currentHeight+10) {
 				renderer.enabled = true;
 			} 
 		else
