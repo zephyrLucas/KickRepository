4accd3137d40af9d7f7ff7abf3855e613993fc46
zephyrLucas <zephyr@lucas.net>
2014-02-14 12:44:31 -0500

    Ouya_Started

--- SuperKick/Assets/Players/Player1.cs
+++ SuperKick/Assets/Players/Player1.cs
@@ -15,42 +15,42 @@ public class Player1 : Player {
 		//uncomment that to make the game end when he dies.
 		}
 	protected override bool isMovingRight() {
-		if(Input.GetKey(KeyCode.D)) {
+		if(Input.GetKey(KeyCode.D) || OuyaExampleCommon.GetAxis(OuyaSDK.AXIS_LSTICK_X, OuyaSDK.OuyaPlayer.player1) > .01f) {
 			return true;
 		}
 		return false;
 	}
 
 	protected override bool isMovingLeft() {
-		if(Input.GetKey(KeyCode.A)) {
+		if(Input.GetKey(KeyCode.A || OuyaExampleCommon.GetAxis(OuyaSDK.AXIS_LSTICK_X, OuyaSDK.OuyaPlayer.player1) < -.01f)) {
 			return true;
 		}
 		return false;
 	}
 
 	protected override bool isJumping() {
-		if(Input.GetKeyDown(KeyCode.C)) {
+		if(Input.GetKeyDown(KeyCode.C || OuyaExampleCommon.GetButton(OuyaSDK.BUTTON_O, OuyaSDK.OuyaPlayer.player1)) {
 			return true;
 		}
 		return false;
 	}
 
 	protected override bool isKicking() {
-		if(Input.GetKeyDown(KeyCode.V)) {
+		if(Input.GetKeyDown(KeyCode.V || OuyaExampleCommon.GetButton(OuyaSDK.BUTTON_A, OuyaSDK.OuyaPlayer.player1))) {
 			return true;
 		}
 		return false;
 	}
 
 	protected override bool isUsingBoost1() {
-		if(Input.GetKeyDown(KeyCode.B)) {
+		if(Input.GetKeyDown(KeyCode.B) || OuyaExampleCommon.GetButton(OuyaSDK.BUTTON_Y, OuyaSDK.OuyaPlayer.player1)) {
 			return true;
 		}
 		return false;
 	}
 
 	protected override bool isUsingBoost2() {
-		if(Input.GetKeyDown(KeyCode.N)) {
+			if(Input.GetKeyDown(KeyCode.N) || OuyaExampleCommon.GetButton(OuyaSDK.BUTTON_U, OuyaSDK.OuyaPlayer.player1)) {
 			return true;
 		}
 		return false;
