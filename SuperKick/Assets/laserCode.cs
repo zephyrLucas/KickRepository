c211edb0fbc466655475696098cc39811246c9ec
ChrisHorrigan <bor3d.mad@gmail.com>
2014-02-25 10:31:32 -0500

    sad
    :c

--- SuperKick/Assets/laserCode.cs
+++ SuperKick/Assets/laserCode.cs
@@ -3,24 +3,30 @@ using System.Collections;
 
 public class laserCode : MonoBehaviour {
 	public Vector2 current;
-	public Vector2 direction;
+	public Vector2 directiono;
 	// Use this for initialization
+	private Animator animator;
 	void Start () {
 		current = transform.localPosition;
-		direction.y = 4;
+		directiono.y = 4;
 		float angle = Random.Range (-3f, 3f);
-		direction.x = angle;
+		directiono.x = angle;
+		animator = this.gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		current += direction*Time.deltaTime;
+		current += directiono*Time.deltaTime;
 		transform.localPosition = current;
 
 		if (Mathf.Abs (transform.localPosition.x) > 24)
 						GameObject.Destroy (this.gameObject);
 				else if (transform.localPosition.y > AlienScript.currentHeight + 20)
 						GameObject.Destroy (this.gameObject);
-
+		RaycastHit contact;
+		if (Physics.Raycast (transform.localPosition, directiono, out contact,1f)) {
+			print (contact.collider);
+			Animator.setTrigger(boomTrig);
+				}
 	}
 }
